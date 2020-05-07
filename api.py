9ca903b81728c40ba1df71a2222b58152d45c74b
Michael West <quagly@gmail.com>
2020-05-07 07:25:27 -0700

    multiple params and returns

--- api.py
+++ api.py
@@ -1,3 +1,16 @@
 # `api.py` file contents
+import re
+
 def string_length(body):
     return {"length": len(body)}, 200
+
+def string_info(body, include_whitespace=True):
+    if include_whitespace:
+        None
+    else:
+        body = re.sub(r"\s+", "", body)
+    return {
+                "length": len(body),
+                "include_whitespace": include_whitespace,
+                "body": body
+           }, 200
