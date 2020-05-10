29396f96d561a7a08161e06572c2950bde0ae03e
Michael West <quagly@gmail.com>
2020-05-10 07:15:03 -0700

    added get-dict/

--- api.py
+++ api.py
@@ -14,3 +14,10 @@ def string_info(body, include_whitespace):
                 "include_whitespace": include_whitespace,
                 "body": body
            }, 200
+
+def get_dict():
+    ret_dict = {
+        "home": 0.0037428185399140637,
+        "government": 0.002829530081728651
+        }
+    return ret_dict, 200
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/local/bin/bash
 
 # when running for reals strict validation is done with
+# for performance probably don't want to validate
 # app.add_api('my_apy.yaml', strict_validation=True)
 
 connexion run swagger.yaml -v --debug --strict-validation
